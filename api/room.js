d84756d872978119c9d200370dfb7440161eec02
yutoi <yuuto_24s1101205@nnn.ed.jp>
2025-12-09 01:45:14 +0900

    修正: Vercel Serverless Functions形式に変更

---
+++ api/room.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const htmlPath = path.join(process.cwd(), 'public', 'room-v2.html');
+  const html = fs.readFileSync(htmlPath, 'utf8');
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(html);
+};
